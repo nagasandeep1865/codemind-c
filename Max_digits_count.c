7f42202271ca40efcbe119a3b538fd2eb698b6c1
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-07-17 09:58:22 +0530

    #include<stdio.h> int digit_count(int n) {     int i,dc=0,d;     while(n)     {         d=n%10;         n=n/10;         dc++;     }     return dc; } int main() {     int n,i,arr[100],p,dc=0,min=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         dc=0;         p=digit_count(arr[i]);         if(min<p)         {             min=p;         }     }     for(i=0;i<n;i++)     {         dc=0;         p=digit_count(arr[i]);         if((digit_count(arr[i]))==min)         {             c++;         }     }     printf("%d",c); }

---
+++ Max_digits_count.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int digit_count(int n)
+{
+    int i,dc=0,d;
+    while(n)
+    {
+        d=n%10;
+        n=n/10;
+        dc++;
+    }
+    return dc;
+}
+int main()
+{
+    int n,i,arr[100],p,dc=0,min=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        dc=0;
+        p=digit_count(arr[i]);
+        if(min<p)
+        {
+            min=p;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        dc=0;
+        p=digit_count(arr[i]);
+        if((digit_count(arr[i]))==min)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
